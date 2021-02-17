4818a65df91a1e51aca9039da97d09e2a03beb73
sandeep <s_k_das@dell.com>
2021-02-18 03:20:54 +0530

    TFS#76345
    Missing File

---
+++ Producer/MenuPublisher/IAppSettings.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenuPublisher
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IAppSettings
+    {
+        void SetConfiguration(IConfiguration configuration);
+        Dictionary<string, object> GetConfigValue();
+    }
+}
