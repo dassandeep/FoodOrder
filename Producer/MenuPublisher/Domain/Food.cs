49b8753bf80dd2816a041c6027dea2fe3d4d64de
sandeep <s_k_das@dell.com>
2021-02-18 03:10:02 +0530

    food

---
+++ Producer/MenuPublisher/Domain/Food.cs
@@ -0,0 +1,21 @@
+﻿
+namespace MenuPublisher
+{
+    using System;
+
+    public class Food
+    {
+        public string ItemName { get; set; }
+        public double ItemPrice { get; set; }
+        public string HotelName { get; set; }
+        //public FoodType foodType { get; set; }
+    }
+    public enum FoodType
+    {
+        Italian, 
+        French, 
+        Greek, 
+        Turkish, 
+        Azian
+    }
+}
